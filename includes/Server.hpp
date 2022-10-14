37ae0246d1a393ad90ffe6c6049102590d40abc1
Gregory GIQUIAUD <ggiquiau@student.42.fr>
2022-10-14 16:03:06 +0200

    epoll test

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -1,25 +1,49 @@
 #ifndef SERVER_HPP
-# define SERVER_HPP
+#define SERVER_HPP
 
+#include <iostream>
 #include <sys/socket.h>
+#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <vector>
+#include <map>
 #include "Client.hpp"
+#include <cstring>
+#include <errno.h>
 
 class Server
 {
+private:
+    std::vector<int> _sockets;
+    int _socket;
+    std::map<int, Client> _clients;
+    int _epfd;
+
+public:
+    Server();
+    ~Server();
+    Server &operator=(Server const cpy);
+    Server(Server const &cpy);
+
+    void createNewSocket(int port);
+    void loop(void);
+
+    int const &getSocket() const;
+    void setSocket(int const domain, int const type, int protocol);
+
+    class ServerException
+    {
     private:
-        std::vector<int> _sockets;
-        int _socket;
-        std::vector<Client> _clients;
-    
+        std::string m_error;
+
     public:
-        Server();
-        ~Server();
-        Server &    operator=(Server const cpy);
-        Server(Server const & cpy);
-        int const & getSocket() const;
-        void setSocket(int const domain, int const type, int protocol);
+        ServerException(const std::string &error)
+            : m_error(error)
+        {
+        }
+
+        const std::string &getError() const { return m_error; }
+    };
 };
 
 #endif
--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -1,6 +1,10 @@
 #include "Server.hpp"
 
-Server::Server() : _socket(0){}
+Server::Server() : _socket(0), _epfd(epoll_create(1))
+{
+    if (_epfd < 0)
+        throw ServerException(strerror(errno));
+}
 
 Server::~Server() {}
 
@@ -14,6 +18,41 @@ Server &    Server::operator=(Server const cpy)
 }
 Server::Server(Server const & cpy) : _socket(cpy._socket)
 {
+}
+
+static int epoll_ctl_add(int epfd, int fd, uint32_t events)
+{
+	struct epoll_event ev;
+	ev.events = events;
+	ev.data.fd = fd;
+	return (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev));
+}
+
+void    Server::createNewSocket(int port)
+{
+    int newSock = socket(AF_INET, SOCK_STREAM, 0);
+    if (newSock < 0)
+        throw ServerException(strerror(errno));
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(newSock, (sockaddr *)&addr, sizeof(addr)) < 0)
+        throw ServerException(strerror(errno));
+    if (listen(newSock, 5) < 0)
+        throw ServerException(strerror(errno));
+    if (epoll_ctl_add(_epfd, newSock, EPOLLIN | EPOLLOUT | EPOLLET) < 0)
+        throw ServerException(strerror(errno));
+}
+
+void    Server::loop()
+{
+    struct epoll_event events[20];
+    while (1)
+    {
+        epoll_wait(_epfd, events, 20, 5000);
+    }
 }
 
 int const & Server::getSocket() const
