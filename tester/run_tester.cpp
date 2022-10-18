947d5db0d9363a2112946841d27c9f6d6ac702a6
gchopin <gchopin@student.42.fr>
2022-10-18 11:11:13 +0200

    push tester sur main branch

---
+++ tester/run_tester.cpp
@@ -0,0 +1,180 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <sys/epoll.h>
+/* man 7 ip */
+
+typedef struct s_message
+{
+	
+}	t_message;
+
+void	ft_close(int fd_socket)
+{
+	if (close(fd_socket) < 0)
+	{
+		std::cerr << "Couldn't close properly" << std::endl;
+		exit(1);
+	}
+}
+
+int	connectSocket(int ft_socket, uint16_t port)
+{
+	struct sockaddr_in	serv_addr;
+	int	ft_connect = 0;
+
+	std::memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	if (serv_addr.sin_addr.s_addr == INADDR_NONE)
+	{
+		ft_close(ft_socket);
+		std::cerr << "Address invalid." << std::endl;
+		return (1);
+	}
+	ft_connect = connect(ft_socket, reinterpret_cast<struct sockaddr *>(&serv_addr), sizeof(serv_addr));
+	if (ft_connect < 0)
+	{
+		ft_close(ft_socket);
+		std::cerr << "Couldn't connect to socket." << std::endl;
+		return (1);
+	}
+	else
+		std::cout << "Connected to socket with "
+			<< port << " port." << std::endl;
+	return (0);
+}
+/*
+static int epoll_ctl_add(int epfd, int fd, uint32_t events)
+{
+	struct epoll_event ev;
+	ev.events = events;
+	ev.data.fd = fd;
+	return (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev));
+}
+*/
+int	main(int argc, char **argv)
+{
+	ssize_t	msg = 0;
+	uint16_t 	port = 0;
+	int	ft_connect = 0;
+	int	ft_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (ft_socket < 0)
+	{
+		std::cerr << "Error socket : " << ft_socket << std::endl;
+		return (1);
+	}
+	if (argc < 2)
+	{
+		ft_close(ft_socket);
+		std::cerr << "please enter a port." << std::endl;
+		return (1);
+	}
+	std::istringstream(argv[1]) >> port;
+	ft_connect = connectSocket(ft_socket, port);
+	if (ft_connect == 1)
+	 	return (1);
+	std::cout << "Sending message..." << std::endl;
+	msg = send(ft_socket, "message", sizeof(char) * 8, 0);
+	if (msg < 0)
+		std::cerr << "Couldn't send message." << std::endl;
+	char buffer[50000];
+	msg = recv(ft_socket, buffer, sizeof(buffer), 0);
+	if (msg < 0)
+	{
+		ft_close(ft_socket);
+		std::cerr << "Couldn't receive message." << std::endl;
+		return (1);
+	}
+	std::cout << buffer << std::endl;
+	//for (unsigned int i = 0; i < 50; ++i)
+	//	std::cout << buffer[i];
+	std::cout << std::endl;
+	ft_close(ft_socket);
+	return (0);
+}
+/*
+int	main(int argc, char **argv)
+{
+	ssize_t	msg = 0;
+	uint16_t 	port = 0;
+	// int	ft_socket = socket(AF_INET, SOCK_STREAM, 0);
+	// int	ft_connect = 0;
+	
+	// if (ft_socket < 0)
+	// {
+	// 	std::cerr << "Error socket : " << ft_socket << std::endl;
+	// 	return (1);
+	// }
+	// if (argc < 2)
+	// {
+	// 	ft_close(ft_socket);
+	// 	std::cerr << "please enter a port." << std::endl;
+	// 	return (1);
+	// }
+	// std::istringstream(argv[1]) >> port;
+	// ft_connect = connectSocket(ft_socket, port);
+	// if (ft_connect == 1)
+	// 	return (1);
+	// std::cout << "Sending message..." << std::endl;
+	*/
+	/*msg = send(ft_socket, "", sizeof("Test message"), 0);
+	if (msg < 0)
+		std::cerr << "Couldn't send message." << std::endl;
+	*/
+	/*
+	int epfd = epoll_create(1);
+	char buffer[50000];
+	struct sockaddr_in cli_addr;
+	int newSock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(9000);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(newSock, (sockaddr *)&addr, sizeof(addr)) < 0)
+        std::cout << "error\n";
+    if (listen(newSock, 5) < 0)
+        std::cout << "error\n";
+    if (epoll_ctl_add(epfd, newSock, EPOLLIN | EPOLLOUT | EPOLLET) < 0)
+        std::cout << "error\n";
+
+	socklen_t s_len (sizeof(cli_addr));
+	int cli_sock;
+	struct epoll_event events[20];
+	while (1)
+	{
+		int nb_fds = epoll_wait(epfd, events, 20, 5000);
+		for (int i = 0;i < nb_fds; ++i)
+		{
+			if (events[i].data.fd == newSock)
+			{
+				cli_sock = accept(events[i].data.fd, (sockaddr *)&cli_addr, &s_len);
+				epoll_ctl_add(epfd, cli_sock, EPOLLIN | EPOLLET | EPOLLRDHUP | EPOLLHUP);
+			}
+			if (events[i].events & EPOLLIN)
+			{
+				msg = recv(cli_sock, buffer, sizeof(buffer), 0);
+				if (msg < 0)
+				{
+					ft_close(cli_sock);
+					std::cerr << "Couldn't receive message." << std::endl;
+					return (1);
+				}
+				for (unsigned int i = 0; i < 50; ++i)
+					std::cout << buffer[i];
+			}
+
+		}
+	
+	}
+	ft_close(cli_sock);
+	return (0);
+}*/
