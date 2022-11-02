729550ff6a13d00a2a4b81fd3ae6bd624982aa98
Gregory GIQUIAUD <ggiquiau@student.42.fr>
2022-11-02 19:03:50 +0100

    config

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string.h>
 #include <string>
 #include <iostream>
 #include <vector>
@@ -8,45 +9,89 @@
 #include <fstream>
 #include <sstream>
 #include <stdint.h>
+#include <algorithm>
+#include <arpa/inet.h>
+#include <limits>
+#include <ctype.h>
 
 class Config
 {
-private:
-	struct location
-	{
-		std::string name;
-		std::string root;
-		std::string index;
-		bool autoindex;
-		std::map<std::string, std::string> fastcgi_param;
-		std::string fastcgi_pass;
-		size_t client_max_body_size;
-		location() : name("/"), root("/var/lib/webserv/html/"), autoindex(false) {}
-	};
+	private:
 
-	struct server
-	{
-		std::string name;
-		std::string host;
-		std::set<uint16_t> listens;
-		std::map<int, std::string> error_pages;
-		std::map<std::string, location> locations;
-		server() : host("127.0.0.1") {}
-	};
+		struct server
+		{
+			std::string server_name;
+			std::string root;
+			std::string fastcgi_pass;
+			bool autoindex;
+			std::vector<std::string> index;
+			std::map<std::string, std::string> fastcgi_param;
+			std::vector<std::pair<std::string, uint16_t> > listens;
+			std::map<int, std::string> error_page;
+			std::map<std::string, server> locations;
+			bool is_location;
+			void init_error_page();
+			static bool assign_port(const std::string &str, uint16_t &val);
+			server() : root("/var/lib/webserv/html/"), autoindex(false), is_location(false) {init_error_page();}
+			const server &operator=(const server &rhs)
+			{
+				server_name = rhs.server_name;
+				root = rhs.root;
+				fastcgi_pass = rhs.fastcgi_pass;
+				autoindex = rhs.autoindex;
+				index = rhs.index;
+				error_page = rhs.error_page;
+				is_location = true;
+				return (*this);
 
-	
+			}
+			friend std::ostream &operator<<(std::ostream &os, const server &rhs)
+			{
+				os << "{\n";
+				os << "server_name: " << rhs.server_name << std::endl;
+				os << "root: " << rhs.root << std::endl;
+				os << "fastcgi_pass: " << rhs.fastcgi_pass << std::endl;
+				os << "autoindex: " << rhs.autoindex << std::endl;
+				for (std::vector<std::pair<std::string, uint16_t> >::const_iterator it = rhs.listens.begin(); it != rhs.listens.end(); ++it)
+					os << "listen: " << it->first << ":" << it->second << std::endl;
+				for (std::map<std::string, server>::const_iterator it = rhs.locations.begin(); it != rhs.locations.end() ; ++it)
+					os << it->first << " " << it->second;
+				os << "}\n";
+				return (os);
+			}
 
-	std::vector<server> _servers;
-	std::string _content;
-	
-	void parse_config();
-	server parse_server(size_t &pos);
+		};
 
-public:
-	Config(const char *conf);
-	Config(const Config &src);
-	~Config();
-	
-	const std::string &getContent() const;
-	const std::vector<server> &getServers() const;
+		void drop_comments();
+		void parse_config();
+		server parse_server(size_t *idx, std::string type, server *parent);
+		std::string get_key(size_t *idx, std::string delimiter);
+		void set_values(server *server, std::string key, std::string value);
+		std::vector<std::string> split(std::string input, char delimiter);
+
+		std::string _content;
+		std::vector<server> _servers;
+
+		class ConfigException: public std::exception
+		{
+		private:
+			std::string m_error;
+
+		public:
+			ConfigException(const std::string &fun, const std::string &error)
+				: m_error(fun + ": " + error)
+			{
+			}
+			~ConfigException() throw(){}
+			const char *what() const throw() { return (m_error.c_str());}
+		};
+
+	public:
+
+		Config(const char *conf);
+		Config(const Config &src);
+		~Config();
+		
+		const std::string &getContent() const;
+		const std::vector<server> &getServers() const;
 };
--- srcs/config/Config.cpp
+++ srcs/config/Config.cpp
@@ -2,18 +2,21 @@
 
 Config::Config(const char *conf)
 {
-	std::ifstream f(conf); // taking file as inputstream
+	std::ifstream f(conf);
 	if (f)
 	{
 		std::ostringstream ss;
-		ss << f.rdbuf(); // reading data
+		ss << f.rdbuf(); 
 		_content = ss.str();
 	}
+	drop_comments();
+	std::cout << _content;
 	parse_config();
-	// std::cout << _content;
+	for (std::vector<server>::iterator it = _servers.begin(); it != _servers.end(); ++it)
+		std::cout << *it;
 }
 
-Config::Config(const Config &src) : _servers(src._servers), _content(src._content) {}
+Config::Config(const Config &src) : _content(src._content), _servers(src._servers) {}
 
 Config::~Config() {}
 
@@ -21,52 +24,174 @@ const std::string &Config::getContent() const
 {
 	return _content;
 }
-void parse_config();
 
 const std::vector<Config::server> &Config::getServers() const
 {
 	return _servers;
 }
 
-void Config::parse_config()
+void Config::drop_comments() 
 {
-	size_t pre = 0;
-	size_t cur = _content.find_first_not_of(" \t\n", pre);
-	if (cur == std::string::npos)
-		return;
-	while (cur != std::string::npos)
+	size_t i = 0;
+	while (i < _content.length())
 	{
-		pre = cur;
-		cur = _content.find_first_of(" \t\n", cur + 1);
-		std::string key = _content.substr(pre, cur - pre);
-		if (key[0] == '#')
-			cur = _content.find("\n", cur);
-		else if (key == "server")
+		if (_content[i] == '#')
 		{
-			Config::server server;
-			_servers.push_back(server);
-			std::cout << cur << std::endl;
+			size_t line = _content.find_first_of("\n",  i + 1);
+			if (line != std::string::npos)
+				_content.erase(i, line - i + 1);
 		}
 		else
-			return;
+			i++;
 	}
+	std::cout << "here" << std::endl;
 }
 
-Config::server Config::parse_server(size_t &pos)
+void Config::parse_config()
 {
-	size_t pre = 0;
-	pos = _content.find_first_not_of(" \t\n", pos);
+	size_t pos = 0;
+	std::string key;
 	while (pos != std::string::npos)
 	{
-		pre = _content.find_first_not_of(" \t\n", pos);
-		pos = _content.find_first_of(" \t\n", pre);
-		std::string key = _content.substr(pre, pos - pre);
-		if (key[0] == '#')
-			pos = _content.find("\n", pos);
-		else if (key == "{")
-		{
-
+		key = get_key(&pos, " \t\n");
+		std::cout << "key " << key << std::endl;
+		if (key == "server")
+			_servers.push_back(parse_server(&pos, "server", NULL));
+		else {
+			std::cout << "error " << key << std::endl;
+			throw ConfigException("incorrect keyword at start of line", strerror(errno));
 		}
 	}
-	return Config::server();
+	if (_servers.empty())
+		throw ConfigException("no server found", strerror(errno));
+}
+
+Config::server Config::parse_server(size_t *idx, std::string type, server *parent)
+{
+	std::string key;
+	size_t pos = *idx;
+	Config::server res;
+
+
+	if (get_key(&pos, " \t\n") != "{")
+		throw ConfigException("open bracket missing in server block", strerror(errno));
+
+	if (parent)
+		res = *parent;
+		
+	while (pos != std::string::npos)
+	{
+		key = get_key(&pos, " \t\n");
+		std::cout << "key: " << key << std::endl;
+		if (key == "}" && (*idx = _content.find_first_not_of(" \n\t", pos + 1)))
+			break;
+		if (type == "server" && key == "location") {
+			std::string name = get_key(&pos, " \t\n");
+			std::cout << "test" << name<< std::endl;
+			res.locations[name] = parse_server(&pos, "location", &res);
+		}
+		else
+		{
+			std::string value = get_key(&pos, ";");
+			std::cout << "value: " << value << std::endl;
+			set_values(&res, key, value);  
+			pos++;
+		}
+	}
+	return res;
+}
+
+std::string Config::get_key(size_t *idx, std::string delimiter)
+{
+	size_t pre = _content.find_first_not_of(" \t\n", *idx);
+	if (pre == std::string::npos)
+		throw ConfigException("parsing error", strerror(errno));
+	size_t pos = _content.find_first_of(delimiter, pre);
+	if (pos == std::string::npos)
+		throw ConfigException("no delimiter found", strerror(errno));
+	std::string key = _content.substr(pre, pos - pre);
+	*idx = pos;
+	return key;
+}
+bool str_is_num(const std::string &str)
+{
+	for (int i = 0; i < str.length(); ++i)
+		if (!std::isdigit(str[i]))
+			return false;
+	return true;
+}
+bool Config::server::assign_port(const std::string &str, uint16_t &val)
+{
+	long l = std::strtol(str.c_str(), NULL, 10);
+	if (l < std::numeric_limits<uint16_t>::min() || l > std::numeric_limits<uint16_t>::max() || errno == ERANGE)
+		return false;
+
+	val = static_cast<uint16_t>(l);
+	return (true);
+}
+void Config::set_values(server *server, const std::string key, const std::string value)
+{
+	
+	if (key == "server_name" && !server->is_location)
+		server->server_name = value;
+	else if (key == "root")
+		server->root = value;
+	else if (key == "fastcgi_pass")
+		server->fastcgi_pass = value;
+	else if (key == "autoindex")
+		server->autoindex = value == "on" ? true : false;
+	else if (key == "fastcgi_param") {}
+	else if (key == "listen" && !server->is_location) {
+		uint16_t port;
+		std::vector<std::string> tmp = split(value, ':');
+		std::cout << "split " << tmp[0] << std::endl;
+		if (tmp.size() == 1)
+		{
+			if (str_is_num(tmp[0])) {
+				if (server::assign_port(value, port)) 
+					server->listens.push_back(std::make_pair("0.0.0.0", port));
+				else				
+					throw ConfigException("incorrect port value", "");
+			}
+			else if (inet_addr(tmp[0].c_str()) != (in_addr_t) -1 ) {
+				std::cout << "valide adrresse " << tmp[0] << std::endl;
+
+				server->listens.push_back(std::make_pair(tmp[0], 80));
+		}
+			else
+				throw ConfigException("invalid ip address", "");
+
+		}
+		else if (tmp.size() == 2 && inet_addr(tmp[0].c_str()) != (in_addr_t) -1 && str_is_num(tmp[1]) && server::assign_port(tmp[1], port))
+		{
+			if (tmp[0].empty())
+				tmp[0] = "0.0.0.0";
+			server->listens.push_back(std::make_pair(tmp[0], port));
+		}
+		else
+			throw ConfigException("listen error", "");
+		
+	}
+	else if (key == "error_page") {
+		// server->error_page[]
+	}
+	else {
+		std::cout << "key error: " << key << std::endl;
+		//throw ConfigException("key error" + key, strerror(errno));
+	}
+}
+void Config::server::init_error_page() {
+	error_page[400]= "response/error_pages/400.html";
+
+}
+
+
+std::vector<std::string> Config::split(std::string input, char delimiter)
+{
+    std::vector<std::string> res;
+    std::stringstream ss(input);
+    std::string str;
+    while (getline(ss, str, delimiter))
+        res.push_back(str);
+    return res;
 }
