6f744124ecf4f088b5401daf154794284a503fae
gchopin <gchopin@student.42.fr>
2022-11-11 15:56:17 +0100

    main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,25 +1,24 @@
 #include "webserv.hpp"
 #include "Server.hpp"
 
-void load_directory_autoindex(std::string &ret_html);
 int main(int argc, char **argv)
 {
+	if (argc <= 1 || argc > 2)
+	{
+		std::cerr << "Need 1 arg" << std::endl;
+		return (1);
+	}
 	Config c(argv[1]);
 
 	try
 	{
 		Server s(argv[1]);
 		s.loop();
-		/* code */
 	}
 	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-
-	// s.createNewSocket(9001);
-	// s.createNewSocket(9002);
-	// s.createNewSocket(80);
 
 	return (0);
 }
